ad86c50ab547103d9ce1288d9cf207b64c793ca1
Timothée Poux <timothee.poux@gmail.com>
2025-12-04 10:01:36 +0100

    Getter des messages des réglages mécaniques

--- common/src/main/java/com/jad/jackytouch/common/IModel.java
+++ common/src/main/java/com/jad/jackytouch/common/IModel.java
@@ -8,4 +8,17 @@ public interface IModel {
     String getCarNeon() throws IOException;
     String getCarRims() throws IOException;
     String getCarSpoiler() throws IOException;
+
+    public String getMessageEsthetique();
+    public String getMessageAerodynamique();
+    public String getMessageExagere();
+    public String getMessageSobre();
+    public String getMessageDisco();
+    public String getMessageAleatoire();
+    public String getMessagePerformance();
+    public String getMessageLowCost();
+    public String getMessageShowOff();
+    public String getMessageDiscret();
+    public String getMessageSport();
+    public String getMessageDrag();
 }
--- model/src/main/java/com/jad/jackytouch/model/Model.java
+++ model/src/main/java/com/jad/jackytouch/model/Model.java
@@ -7,6 +7,7 @@ import java.nio.file.Path;
 import com.jad.jackytouch.common.IModel;
 
 public class Model implements IModel {
+    //Getter des fichers txt
     @Override
     public String getCarBase() throws IOException{
         return Files.readString(Path.of("model/src/main/ressources/car_base.txt"));
@@ -31,4 +32,34 @@ public class Model implements IModel {
     public String getCarSpoiler() throws IOException{
         return Files.readString(Path.of("model/src/main/ressources/spoiler.txt"));
     }
+
+    //Getter des messages des réglages mécaniques
+    //Spoiler
+    @Override
+    public String getMessageEsthetique(){return "aucun effet mécanique.";};
+    @Override
+    public String getMessageAerodynamique(){return "stabilité accrue.";};
+    @Override
+    public String getMessageExagere(){return "vitesse max réduite, effet visuel accentué.";};
+    //Neons
+    @Override
+    public String getMessageSobre(){return "lumière fixe.";};
+    @Override
+    public String getMessageDisco(){return "clignotement simulé.";};
+    @Override
+    public String getMessageAleatoire(){return "affichage imprévisible.";};
+    //Jantes
+    @Override
+    public String getMessagePerformance(){return "accélération améliorée.";};
+    @Override
+    public String getMessageLowCost(){return "aucun effet.";};
+    @Override
+    public String getMessageShowOff(){return "bruit distinctif.";};
+    //Exhaust
+    @Override
+    public String getMessageDiscret(){return "bruit discret.";};
+    @Override
+    public String getMessageSport(){return "bruit puissant.";};
+    @Override
+    public String getMessageDrag(){return "bruit extrême.";};
 }
