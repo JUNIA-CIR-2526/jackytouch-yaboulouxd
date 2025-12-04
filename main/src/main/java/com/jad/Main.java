c746f86ab5039fefcd0f8011664d85537de59cfc
Timothée Poux <timothee.poux@gmail.com>
2025-12-04 09:27:55 +0100

    fonctions de model

--- main/src/main/java/com/jad/Main.java
+++ main/src/main/java/com/jad/Main.java
@@ -1,7 +1,11 @@
 package com.jad;
 
+import com.jad.jackytouch.common.IModel;
+import com.jad.jackytouch.model.Model;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.printf("Hello and welcome!");
+        IModel model = new Model();
+
     }
 }
--- model/src/main/java/com/jad/jackytouch/model/Model.java
+++ model/src/main/java/com/jad/jackytouch/model/Model.java
@@ -1,4 +1,35 @@
 package com.jad.jackytouch.model;
 
-public class Model {
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+import com.jad.jackytouch.common.IModel;
+
+public class Model implements IModel {
+    @Override
+    public String getCarBase() throws IOException{
+        return Files.readString(Path.of("ressources/car_base.txt"));
+    }
+
+    @Override
+    public String getExhaust() throws IOException{
+        return Files.readString(Path.of("ressources/exhaust.txt"));
+    }
+
+    @Override
+    public String getCarNeon() throws IOException{
+        return Files.readString(Path.of("ressources/neon.txt"));
+    }
+
+    @Override
+    public String getCarRims() throws IOException{
+        return Files.readString(Path.of("ressources/rims.txt"));
+    }
+
+    @Override
+    public String getCarSpoiler() throws IOException{
+        return Files.readString(Path.of("ressources/spoiler.txt"));
+    }
 }
