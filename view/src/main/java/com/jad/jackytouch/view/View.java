3884b31375991803d385c424ff2a3756efd4e866
eloise <eloise.ansoult@student.junia.com>
2025-12-04 10:01:43 +0100

    View,Controller,Main

--- view/src/main/java/com/jad/jackytouch/view/View.java
+++ view/src/main/java/com/jad/jackytouch/view/View.java
@@ -1,4 +1,12 @@
 package com.jad.jackytouch.view;
 
-public class View {
+import com.jad.jackytouch.common.IView;
+
+public class View implements IView {
+
+    @Override
+    public void displayCarBase(final String carsBase){
+        System.out.println(carsBase);
+    }
+
 }
